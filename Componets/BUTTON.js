2114647cbca26beb13aa79ce0809727268e3863b
Javier  Carreño <35305955+car251290@users.noreply.github.com>
2020-10-10 13:56:11 -0700

    header and button

--- Componets/BUTTON.js
+++ Componets/BUTTON.js
@@ -1,34 +1,34 @@
 import { red } from '@material-ui/core/colors';
 import React from 'react';
 import { StyleSheet, Text, View, Button,Dimensions  } from 'react-native';
+//import { Button } from 'react-native-paper';
 
 const BUTTON = (props) => {
 
-    return (
-      <View style={ style.button }>
-         
-         <Button title = 'Less get a Beer'
-  
-  onPress={() => {
-    WebBrowser.openBrowserAsync('https://www.google.com/maps/@49.2125932,-123.0931223,14z');
-  }} 
- />  
-      </View>
-    )
-  };
-  
-  const style = StyleSheet.create({
-    button: {
-      flex:.1,
-      marginTop:60,
-      alignItems: 'center',
-      padding: 1.5,
-      width: 150,
-      height: 10,
-      borderRadius:7,
-      color:'#fff',fontSize:40,
-      backgroundColor: '#a9a9a9',
-    }, 
-  });
+  return (
+    <View style={ style.button }>
+       
+       <Button title = 'Less get a Beer'
+
+onPress={() => {
+  WebBrowser.openBrowserAsync('https://www.google.com/maps/@49.2125932,-123.0931223,14z');
+}} 
+/>  
+    </View>
+  )
+};
+
+const style = StyleSheet.create({
+  button: {
+    flex:.2,
+    marginTop:40,
+    alignItems: 'center',
+    width: 150,
+    height: 10,
+    borderRadius:7,
+    color:'#ab003c',fontSize:40,
+    backgroundColor: '#a9a9a9',
+  }, 
+});
 
   export default BUTTON;
--- Componets/Header.js
+++ Componets/Header.js
@@ -5,7 +5,10 @@ import { StyleSheet, Text, View,Image} from 'react-native';
 export default function Header() {
 return (
     <View style={styles.container}>
+      <Image source={require('./Componets/Images/typesofbeers.jpg')}  style={styles.backgroundimage}/>
+
          <Text style ={styles.headerText}>It is beer time!</Text>
+
          <Image size={24} source={require('./Images/beer-vector.jpg')}  style={styles.backgroundiconbeer}/>
         <Image size={24} source={require('./Images/home.jpg')}  style={styles.backgroundIcon}/>
       
@@ -31,11 +34,17 @@ const styles = StyleSheet.create({
       },
       backgroundiconbeer: {
         alignItems: 'center',
-        top:20,
+        top:15,
         width:100,
         height:120,
-        marginBottom:20,
-        borderColor:'blue',
+        marginBottom:25,
+        borderColor:'#3f51b5',
+      },
+      backgroundimage:{
+        flex: 2,
+        alignItems: 'center',
+        width:400,
+        height:630,
       },
   });
 
